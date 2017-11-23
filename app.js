b26954c1957d3992393ab4e9b76e6f289d5de425
Steffi Marie Seniedo <steffimarie@wifi-172-23-56-171.uoa-wifi.auckland.ac.nz>
2017-11-23 15:49:36 +1300

    Add project files and folders

--- app.js
+++ app.js
@@ -11,8 +11,8 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
 
 // Create chat connector for communicating with the Bot Framework Service
 var connector = new builder.ChatConnector({
-    appId: process.env.MICROSOFT_APP_ID,
-    appPassword: process.env.MICROSOFT_APP_PASSWORD
+    appId: "061024b0-7892-4540-b32f-ce165ad654df",
+    appPassword: "lyfJSHY7}@_rjqdRCT4287("
 });
 
 // Listen for messages from users 
